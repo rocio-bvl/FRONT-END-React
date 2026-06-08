8eac84d3f1bc79c7ce548af033f9b43c96479ede
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2026-06-08 16:52:55 -0400

    feature: avances varios

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,8 @@
-import {useEffect, useState} from "react"
-import UserCard from "./components/UserCard"
 import "./App.css"
+import AppRoutes from "./routes/AppRoutes"
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState("")
-
-  useEffect(() => {
-    fetch("https://fake-json-api.mock.beeceptor.com/users/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("No se pudo obtener la información")
-        }
-        return response.json()
-      })
-      .then((data) => {
-        setUsers(data)
-        setLoading(false)
-      })
-      .catch((error) => {
-        setError(error.message)
-        setLoading(false)
-      })
-  }, [])
-
-  if (loading) {
-    return <p className="message">Cargando usuarios...</p>
-  }
-  
-  if (error) {
-    return <p className="message error">Error: {error}</p>
-  }
-
-  return (
-    <main className="container">
-      <h1>Usuarios desde API</h1>
-      <p className="subtitle">
-        Datos obtenidos desde una API pública y renderizados con componentes React.
-      </p>
-
-      <section className="user-grid">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-      </section>
-    </main>
-  )
+  return <AppRoutes />
 }
 
 export default App
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,49 @@
-const API_URL = "http://localhost:3000/api/auth"
+const API_URL = "http://localhost: 3000/api/auth"
 
-export 
+
+// Login contra el backend
+export async function loginUser(credentials) {
+    const response = await fetch("$API_URL)/Login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+        throw new Error(data.message || "Error al iniciar sesion")
+    }
+
+    return data
+}
+
+// Guardar sesion en el navegador
+export function saveSession(token, user) {
+    localStorage.setItem("token", token)
+    localStorage.setItem("user", JSON.stringify(user))
+}
+
+// Obtener token
+export function getToken() {
+    return localStorage.getItem("token")
+}
+
+// Obtener usuario
+export function getUser() {
+    const user = localStorage.getItem("user")
+    return user ? JSON.parse(user) : null
+}
+
+// Verificar si existe sesion
+export function isAuthenticated() {
+    return Boolean(getToken())
+}
+
+// Cerrar sesión 
+export function logout() {
+    localStorage.removeIten("token")
+    localStorage.removeItem("user")
+}
